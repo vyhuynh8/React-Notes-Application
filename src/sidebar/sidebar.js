d0f7b0a188c3844ccb4794155561d8068dd8dee4
Vy Huynh <vy@gatech.edu>
2019-10-18 17:06:53 -0400

    Built sidebar items (it can retrieve from firebase database now)

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -19,29 +19,77 @@ class SidebarComponent extends React.Component {
 
 		const { notes, classes, selectedNoteIndex } = this.props;
 
-
-
-
+		if (!notes) {
+			return (<div></div>);
+		}
 
 		return(
 			<div className={classes.sidebarContainer}>
 			<Button 
 				onClick={this.newNoteBtnClick} 
 				className={classes.newNoteBtn}>
-			New Note
+			{this.state.addingNote ? "Cancel" : "New Note"}
 			</Button>
 			{
 				this.state.addingNote ? 
-				<div>Note has been added! :)</div> : null
+				<div>
+				<input 
+					type='text' 
+					className={classes.newNoteInput} 
+					placeholder="Enter note title"
+					onKeyUp={ (e) => this.updateTitle(e.target.value)}>
+				</input>
+				<Button 
+					className={classes.newNoteSubmitBtn}
+					onClick={this.newNote}> Submit Note</Button>
+				</div> : null
 			}
-			</div>);
-		}
+			<List>
+				{
+					notes.map((_note, _index) => {
+						return(
+						<div key={_index}>
+							<SidebarItemComponent
+								_note={_note}
+								_index={_index}
+								selectedNoteIndex={selectedNoteIndex}
+								selectNote={this.selectNote}
+								deleteNote={this.deleteNote}>
+							</SidebarItemComponent>
+							<Divider></Divider>
+						</div>
+						)
+					})
+				}
+			</List>
+			</div>
+		);
+
+	}
 
 
 		newNoteBtnClick = () => {
+			this.setState( { title: null, addingNote: !this.state.addingNote });
 			console.log("NEW BTN CLICKED");
 		}
 
+		updateTitle = (txt) => {
+			this.setState({ title: txt })
+			console.log("HERE IT IS: ", txt)
+		}
+
+		newNote = () => {
+			console.log(this.state);
+		}
+
+		selectNote = () => {
+			console.log("select note");
+		}
+
+		deleteNote = () => {
+			console.log("delete note");
+		}
+ 
 
 }
 
--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -8,12 +8,39 @@ import { removeHTMLTags } from '../helpers';
 
 
 class SidebarItemComponent extends React.Component {
-	constructor() {
-		super();
-	}
 
 	render() {
-		return(<div>Hello from the sidebar item!</div>);
+
+		const{ _index, _note, classes, selectedNoteIndex } = this.props;
+		
+		return(
+			<div key={_index}>
+				<ListItem
+					className={classes.ListeItem}
+					selected={selectedNoteIndex === _index}
+					alignItems='flex-start'>
+						<div 
+							className={classes.textSection}
+							onClick = { () => this.selectNote(_note, _index)}>
+							<ListItemText
+								primary={_note.title}
+								secondary={removeHTMLTags(_note.body.substring(0,30))+"..."}>
+							</ListItemText>
+						</div>
+						<DeleteIcon 
+							onClick= {() => this.deleteNote(_note)}
+							className={classes.deleteIcon}>
+						</DeleteIcon>
+				</ListItem>
+			</div>
+		);
+	}
+
+	selectNote = (n,i) => { this.props.selectNote(n,i);}
+	deleteNote = (n) => {
+		//backticks allow you to render javascript
+		if (window.confirm(`Are you sure you want to delete: ${n.title}`))
+			this.props.deleteNote(n);
 	}
 }
 
